833fb977dbd5b797cc787c8e6fc4ec400372b73f
Sarffk17 <Ksarff17@gmail.com>
2024-06-30 20:40:52 -0400

    Updated JS
    Updated weather app to properly pull data per city input

--- src/script.js
+++ src/script.js
@@ -1,8 +1,19 @@
+function updateWeatherData(response) {
+    console.log(response.data);
+}
+
+function searchCity(city) {
+    let apiKey = "4tao58d1a814447011bb9de5e3623b3f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(updateWeatherData);
+}
+
 function searchNewCityData(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#searchFormInput");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 } 
 
 let searchFormElement = document.querySelector("#searchForm");
